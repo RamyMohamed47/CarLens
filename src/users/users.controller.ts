49d1781422783b287275c32f644fe31b31b5842f
Ramy Mohamed <mohamedramy228@ymail.com>
2026-08-30 17:55:11 +0300

    feat: typeorm and project foundation

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDTO) {
+        console.log(body);
+    }
+}
