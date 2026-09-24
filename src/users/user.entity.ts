6d7a7aadcca57181a9b3b0bdfff640bac82299c9
Ramy Mohamed <mohamedramy228@ymail.com>
2026-09-24 17:01:12 +0300

    feat: db relations

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,8 +5,9 @@ import {
   AfterInsert,
   AfterRemove,
   AfterUpdate,
+  OneToMany,
 } from 'typeorm';
-
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -18,6 +19,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
